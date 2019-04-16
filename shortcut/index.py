bd8dbc92fdd327de34b2c3c8b6bc6272ca5935bb
Yasser Hawass <yasser.hawass@gmail.com>
2019-04-16 18:27:28 +0200

    before the refactoring commit

--- shortcut/index.py
+++ shortcut/index.py
@@ -33,38 +33,47 @@
 #     if any([key in COMBO for COMBO in COMBINATIONS]):
 #         current.remove(key)
 #################################################################################################################
-from pynput import keyboard
+from pynput import keyboard as pynputkeyboard
+import keyboard         #external file
 #from subprocess import call
 
 # The key combination to check
 COMBINATIONS = [
-    {keyboard.Key.shift, keyboard.KeyCode(char='a')},
-    {keyboard.Key.shift, keyboard.KeyCode(char='A')}
+    {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='a')},
+    {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='A')}
 ]
 
-def VolumeUp():
-    try:
-        print("VolumeUp")
-            #call(["amixer", "-D", "pulse", "sset", "Master", "5%+"])
-    except ValueError:
-        pass
+COMBINATIONZ = [
+    {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='z')},
+    {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='Z')}
+]
+
+MUTE = [
+    {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='q')},
+    {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='Q')}
+]
 
 # The currently active modifiers
 current = set()
 
-def execute():
-    print ("Do Something")
-    VolumeUp()
 
 def on_press(key):
-    if any([key in COMBO for COMBO in COMBINATIONS]):
+    if any([key in COMBO for COMBO in COMBINATIONS]) or any([key in COMBO for COMBO in COMBINATIONZ]) or any([key in COMBO for COMBO in MUTE]):
         current.add(key)
         if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
-            execute()
+            keyboard.VolumeUp()
+        elif any(all(k in current for k in COMBO) for COMBO in COMBINATIONZ):
+            keyboard.VolumeDown()
+        elif any(all(k in current for k in COMBO) for COMBO in MUTE):
+            keyboard.Mute()
 
 def on_release(key):
     if any([key in COMBO for COMBO in COMBINATIONS]):
         current.remove(key)
+    elif any([key in COMBO for COMBO in COMBINATIONZ]):
+        current.remove(key)
+    elif any([key in COMBO for COMBO in MUTE]):
+        current.remove(key)
 
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+with pynputkeyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
