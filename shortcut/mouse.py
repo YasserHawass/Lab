d4447371c3aa5696c9f9feb573fbaa5c91f724fb
Yasser Hawass <yasser.hawass@gmail.com>
2019-04-17 04:27:00 +0200

    refactorin mouse.py

--- shortcut/mouse.py
+++ shortcut/mouse.py
@@ -1,26 +1,6 @@
-from pynput import keyboard as pynputkeyboard
-import keyboard         #external file
 from pynput import mouse
 
-
-
-def on_functionf8(key):
-    if (key==pynputkeyboard.Key.f1):
-        print('f8 is pressed')
-
-
-key_listener = pynputkeyboard.Listener(on_release=on_functionf8)
-key_listener.start()
-
-
 def on_click(x, y, button, pressed):
-    print("we in")
-    if button == mouse.Button.left:
-        print ('Left')
-
-    if button == mouse.Button.right:
-        key_listener.stop()
-        print ('right')
     if button == mouse.Button.middle:
         print ('middle')
 
