f172638c353773042a99d9f2a0904d0c0e361544
Yasser Hawass <yasser.hawass@gmail.com>
2019-04-17 04:28:53 +0200

    refactor index.py

--- shortcut/index.py
+++ shortcut/index.py
@@ -1,7 +1,5 @@
-#https://nitratine.net/blog/post/how-to-make-hotkeys-in-python/
 from pynput import keyboard as pynputkeyboard
 import keyboard         #external file
-
 
 # The key combination to check
 COMBINATIONS = [
@@ -27,7 +25,6 @@ MEDIA = [
 # The currently active modifiers
 current = set()
 def on_press(key):
-    #print(key)                                                       #recorder for keystrokes
     #update here 1
     if any([key in COMBO for COMBO in COMBINATIONS]) or any([key in COMBO for COMBO in COMBINATIONZ]) or any([key in COMBO for COMBO in MUTE]) or any([key in COMBO for COMBO in MEDIA]):
         current.add(key)
@@ -45,16 +42,67 @@ def on_release(key):
     #update here 3
     if any([key in COMBO for COMBO in COMBINATIONS]) or any([key in COMBO for COMBO in COMBINATIONZ]) or any([key in COMBO for COMBO in MUTE]) or any([key in COMBO for COMBO in MEDIA]):
         current.remove(key)
-    #keep it for later use and such
-    # elif any([key in COMBO for COMBO in COMBINATIONZ]):
-    #     current.remove(key)
-    # elif any([key in COMBO for COMBO in MUTE]):
-    #     current.remove(key)
-    # elif any([key in COMBO for COMBO in MEDIA]):
-    #     current.remove(key)
-
 with pynputkeyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
+
+
+#https://nitratine.net/blog/post/how-to-make-hotkeys-in-python/
+# from pynput import keyboard as pynputkeyboard
+# import keyboard         #external file
+#
+#
+# # The key combination to check
+# COMBINATIONS = [
+#     {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='a')},
+#     {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='A')}
+# ]
+#
+# COMBINATIONZ = [
+#     {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='z')},
+#     {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='Z')}
+# ]
+#
+# MUTE = [
+#     {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='q')},
+#     {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='Q')}
+# ]
+#
+# MEDIA = [
+#     {pynputkeyboard.Key.shift, pynputkeyboard.KeyCode(char='1')}
+# ]
+#
+# #http://cherrytree.at/misc/vk.htm
+# # The currently active modifiers
+# current = set()
+# def on_press(key):
+#     #print(key)                                                       #recorder for keystrokes
+#     #update here 1
+#     if any([key in COMBO for COMBO in COMBINATIONS]) or any([key in COMBO for COMBO in COMBINATIONZ]) or any([key in COMBO for COMBO in MUTE]) or any([key in COMBO for COMBO in MEDIA]):
+#         current.add(key)
+#         #update here 2, add the func in keyboard 4
+#         if any(all(k in current for k in COMBO) for COMBO in COMBINATIONS):
+#             keyboard.VolumeUp()
+#         elif any(all(k in current for k in COMBO) for COMBO in COMBINATIONZ):
+#             keyboard.VolumeDown()
+#         elif any(all(k in current for k in COMBO) for COMBO in MUTE):
+#             keyboard.Mute()
+#         elif any(all(k in current for k in COMBO) for COMBO in MEDIA):
+#             keyboard.Play()
+#
+# def on_release(key):
+#     #update here 3
+#     if any([key in COMBO for COMBO in COMBINATIONS]) or any([key in COMBO for COMBO in COMBINATIONZ]) or any([key in COMBO for COMBO in MUTE]) or any([key in COMBO for COMBO in MEDIA]):
+#         current.remove(key)
+#     #keep it for later use and such
+#     # elif any([key in COMBO for COMBO in COMBINATIONZ]):
+#     #     current.remove(key)
+#     # elif any([key in COMBO for COMBO in MUTE]):
+#     #     current.remove(key)
+#     # elif any([key in COMBO for COMBO in MEDIA]):
+#     #     current.remove(key)
+#
+# with pynputkeyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+#     listener.join()
 
 
 
