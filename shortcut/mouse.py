f8a807d7556e9bfc7ad962e7d1d00613f635276c
Yasser Hawass <yasser.hawass@gmail.com>
2019-04-17 05:37:19 +0200

    fixing dup in mouse.py

--- shortcut/mouse.py
+++ shortcut/mouse.py
@@ -1,8 +1,21 @@
-from pynput import mouse
+from pynput import mouse as pynputmouse
+
+# test = [
+#     {pynputmouse.Button.right, pynputmouse.Button.left}
+# ]
+# currentm = set()
 
 def on_click(x, y, button, pressed):
-    if button == mouse.Button.middle:
-        print ('middle')
+    if pressed:
+        if button == pynputmouse.Button.middle:
+            print ('middle')
+        # elif any(all(k in currentm for k in COMBO) for COMBO in test):
+        #     print ('middleX')
+    # elif (pynputmouse.Button.middle and pynputmouse.Button.left):
+    #         print("Woah")
+    # elif not pressed:
+    #     # Stop listener
+    #     return False              stops the app
 
-with mouse.Listener(on_click=on_click) as MouseListener:
+with pynputmouse.Listener(on_click=on_click) as MouseListener:
         MouseListener.join()
